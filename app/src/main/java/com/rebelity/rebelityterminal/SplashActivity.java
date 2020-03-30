c55423399b8b6f3b0cba59464cc9e442bac14402
ITDevStar <itdevstar@hotmail.com>
2020-03-30 22:35:07 +0300

    splash and icon

--- app/src/main/java/com/rebelity/rebelityterminal/SplashActivity.java
+++ app/src/main/java/com/rebelity/rebelityterminal/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         startActivity(new Intent(SplashActivity.this, LoginActivity.class));
 
